06d72d554951db9cb006b1eddee170d2e52447a2
Liviu Mandras-Iura <liviu.mandras-iura@leaseplan.com>
2024-01-20 08:11:46 +0200

    green: implemented validations

--- DiamondKata/DiamondKata/DiamondCreator.cs
+++ DiamondKata/DiamondKata/DiamondCreator.cs
@@ -4,13 +4,20 @@ public class DiamondCreator
 {
 	public char[,] CreateDiamond(char midpoint)
 	{
+		if (midpoint < 'A' || 'Z' < midpoint)
+		{
+			// in the real world something like this could be handled with a Result<T> depending on scenario and location in the architecture 
+			throw new ArgumentException("Must be a capital letter of the English alphabet", nameof(midpoint));
+		}
+
 		if (midpoint == 'A')
 		{
 			var diamondModel = new char[1, 1];
 			diamondModel[0, 0] = 'A';
-			
+
 			return diamondModel;
 		}
+
 		return null;
 	}
 }
