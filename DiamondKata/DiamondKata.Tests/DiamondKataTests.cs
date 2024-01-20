0596961804278c391d8cdda1ba5154d01c2229ca
Liviu Mandras-Iura <liviu.mandras-iura@leaseplan.com>
2024-01-20 07:52:48 +0200

    red: handle special case when diamond is a dot

--- DiamondKata/DiamondKata.Tests/DiamondKataTests.cs
+++ DiamondKata/DiamondKata.Tests/DiamondKataTests.cs
@@ -16,4 +16,19 @@ public class DiamondKataTests
 		//assert
 		diamond.Should().BeNull();
 	}
+
+	[Fact]
+	public void When_MidpointIsA_Then_DiamondIsADot()
+	{
+		//arrange
+		var creator = new DiamondCreator();
+
+		//act
+		char[,] diamond = creator.CreateDiamond('A');
+
+		//assert
+		diamond.GetLength(0).Should().Be(1);
+		diamond.GetLength(1).Should().Be(1);
+		diamond[0, 0].Should().Be('A');
+	}
 }
