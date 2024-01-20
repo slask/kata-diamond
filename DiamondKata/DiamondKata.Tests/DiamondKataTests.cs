64599c2042dac0f4a9699b19542c8321ab107416
Liviu Mandras-Iura <liviu.mandras-iura@leaseplan.com>
2024-01-20 08:09:10 +0200

    red: input validations

--- DiamondKata/DiamondKata.Tests/DiamondKataTests.cs
+++ DiamondKata/DiamondKata.Tests/DiamondKataTests.cs
@@ -27,4 +27,14 @@ public class DiamondKataTests
 		diamond.GetLength(1).Should().Be(1);
 		diamond[0, 0].Should().Be('A');
 	}
+
+	[Fact]
+	public void When_MidpointIsNotACapitalLetter_ThrowArgumentException()
+	{
+		Action createLeft = () => _creator.CreateDiamond((char)('A' - 1));
+		createLeft.Should().Throw<ArgumentException>();
+
+		Action createRight = () => _creator.CreateDiamond((char)('Z' + 1));
+		createRight.Should().Throw<ArgumentException>();
+	}
 }
