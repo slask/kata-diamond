9b9ff391cfe71088e293b5990e93ef22319e7f5b
Liviu Mandras-Iura <liviu.mandras-iura@leaseplan.com>
2024-01-23 11:35:25 +0200

    feat: read input from command line

--- DiamondKata/DiamondKata/Program.cs
+++ DiamondKata/DiamondKata/Program.cs
@@ -2,8 +2,20 @@
 
 using DiamondKata;
 
+if (args.Length != 1)
+{
+	Console.WriteLine("The diamond midpoint letter must be provided in the arguments.");
+	return;
+}
+
+if (args[0].Length > 1)
+{
+	Console.WriteLine("The midpoint must be a single capital letter.");
+	return;
+}
+
 var creator = new DiamondCreator();
-char[,] diamond = creator.CreateDiamond('H', ' ');
+char[,] diamond = creator.CreateDiamond(args[0][0], ' ');
 
 PrintDiamondModel(diamond);
 
